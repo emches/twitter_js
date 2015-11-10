0847a11e4b2e1503026a52efd7d9ddfd26b3d08a
MarkWaldron <markewaldron@gmail.com>
2015-11-10 14:23:38 -0500

    Swig renderer working with basic test data

--- app.js
+++ app.js
@@ -4,17 +4,7 @@ var swig = require('swig');
 var app = express(); // creates an instance of an express application
 var port = 3000;
 var loggerMessage = chalk.bold.green;
-
-var data = {
-    title: 'An Example',
-    people: [{
-        name: 'Gandalf',
-    }, {
-        name: 'Frodo'
-    }, {
-        name: 'Hermione'
-    }]
-};
+var people = [{name: 'Mark Waldron'}, {name: 'Emily Chesler'}, {name: 'David Yang'}, {name: 'Nimit Maru'}, {name: 'Victor Hom'}];
 
 // Logging Middleware
 app.use(function(req, res, next){
@@ -26,10 +16,11 @@ app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
 
 // Handles root route, renders index template
 app.get('/', function (req, res) {
-  res.render('index', data);
+  res.render('index', {title: 'Hall of Fame', people: people});
 });
 
 // Starts server on specified port
