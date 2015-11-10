7ee6320066c3ffeaca3e79d62194a132a0fc3bcf
MarkWaldron <markewaldron@gmail.com>
2015-11-10 14:16:03 -0500

    Implemented swig renderer on index

--- app.js
+++ app.js
@@ -4,23 +4,35 @@ var swig = require('swig');
 var app = express(); // creates an instance of an express application
 var port = 3000;
 var loggerMessage = chalk.bold.green;
-//Logging Middleware
+
+var data = {
+    title: 'An Example',
+    people: [{
+        name: 'Gandalf',
+    }, {
+        name: 'Frodo'
+    }, {
+        name: 'Hermione'
+    }]
+};
+
+// Logging Middleware
 app.use(function(req, res, next){
   console.log("Method: " + loggerMessage(req.method) + " Path: " + loggerMessage(req.path));
   next();
 });
 
+app.engine('html', swig.renderFile);
 
-app.get('/', function(req, res, next ){
-  res.send("HELLO WELCOME FRIEND");
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Handles root route, renders index template
+app.get('/', function (req, res) {
+  res.render('index', data);
 });
 
-
-
-
-
-
-
+// Starts server on specified port
 app.listen(port, function(){
-    console.log("listening to port: " + port);
+  console.log("Listening on port: " + port);
 });
